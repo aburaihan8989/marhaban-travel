c902d5f2332285002f04150107c2e07b799b78d6
aburaihan8989 <yusup.smartphone@gmail.com>
2024-04-22 16:52:48 +0700

    initial

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Inventory System © {{ date('Y') }} || <strong><a target="_blank" href="#"><i>Marhaban Makkah Madinah - Inventory System</i></a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
