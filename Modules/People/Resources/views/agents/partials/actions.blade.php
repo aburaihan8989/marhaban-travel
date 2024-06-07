5438db6d218633e54df53277b9088e1bd2f2622e
aburaihan8989 <yusup.smartphone@gmail.com>
2024-06-07 23:06:03 +0700

    update

--- Modules/People/Resources/views/agents/partials/actions.blade.php
+++ Modules/People/Resources/views/agents/partials/actions.blade.php
@@ -2,23 +2,40 @@
     <a href="{{ route('agents.edit', $data->id) }}" class="btn btn-info btn-sm">
         <i class="bi bi-pencil"></i>
     </a>
-{{-- @endcan
-@can('show_customers') --}}
+{{-- @endcan --}}
+{{-- @can('show_customers') --}}
     <a href="{{ route('agents.show', $data->id) }}" class="btn btn-primary btn-sm">
         <i class="bi bi-eye"></i>
     </a>
-{{-- @endcan
-@can('delete_customers') --}}
-    <button id="delete" class="btn btn-danger btn-sm" onclick="
-        event.preventDefault();
-        if (confirm('Are you sure? It will delete the data permanently!')) {
-        document.getElementById('destroy{{ $data->id }}').submit()
-        }
-        ">
-        <i class="bi bi-trash"></i>
-        <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('agents.destroy', $data->id) }}" method="POST">
-            @csrf
-            @method('delete')
-        </form>
+{{-- @endcan --}}
+@if ( $data->id == 1)
+{{-- @can('delete_customers') --}}
+    <button id="delete" class="btn btn-secondary btn-sm" onclick="
+    // event.preventDefault();
+    // if (confirm('Are you sure? It will delete the data permanently!')) {
+    // document.getElementById('destroy{{ $data->id }}').submit()
+    // }
+    // ">
+    <i class="bi bi-trash"></i>
+    {{-- <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('agents.destroy', $data->id) }}" method="POST">
+        @csrf
+        @method('delete')
+    </form> --}}
     </button>
 {{-- @endcan --}}
+@else
+{{-- @can('delete_customers') --}}
+    <button id="delete" class="btn btn-danger btn-sm" onclick="
+    event.preventDefault();
+    if (confirm('Are you sure? It will delete the data permanently!')) {
+    document.getElementById('destroy{{ $data->id }}').submit()
+    }
+    ">
+    <i class="bi bi-trash"></i>
+    <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('agents.destroy', $data->id) }}" method="POST">
+        @csrf
+        @method('delete')
+    </form>
+    </button>
+{{-- @endcan --}}
+@endif
