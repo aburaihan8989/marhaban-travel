a4076463b0cc345e043ec9480b408fb350a31ab7
aburaihan8989 <yusup.smartphone@gmail.com>
2024-06-20 14:26:21 +0700

    update

--- Modules/People/Resources/views/agents/payments/create.blade.php
+++ Modules/People/Resources/views/agents/payments/create.blade.php
@@ -28,7 +28,7 @@
                                 <div class="col-lg-6">
                                     <div class="form-group">
                                         <label for="reference">Reference ID <span class="text-danger">*</span></label>
-                                        <input type="text" class="form-control" name="reference" required readonly value="INV/{{ $agent->agent_code }}">
+                                        <input type="text" class="form-control" name="reference" required readonly value="INV/DB/{{ $agent->agent_code }}">
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
